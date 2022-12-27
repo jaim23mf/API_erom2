4f3885cf0e7c47a7fd62687d7b271757626c1de9
jaim23mf <108651419+jaim23mf@users.noreply.github.com>
2022-12-27 14:11:53 +0100

    Bug Fix
    Editing Service problem #4

--- euroma2/Models/Map/FloorSaveInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace euroma2.Models.Map
-{
-    public class FloorSaveInfo
-    {
-        public List<Floor_Nav_Point> navPoints { get; set; }
-        public List<Floor_Shop> shopsNodes { get; set; }
-
-    }
-}
--- euroma2/Models/Service/Service_model.cs
+++ euroma2/Models/Service/Service_model.cs
@@ -7,7 +7,7 @@ namespace euroma2.Models.Service
         public int id { get; set; }
         public string title { get; set; }
         public string description { get; set; }
-        public int order { get; set; }
+        public int? order { get; set; }
         public string icon { get; set; }
 
     }
